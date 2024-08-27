5fe10fd6578d2e30cdf13637f62804382d7e737f
akshaya-cp <akshayasingh61@gmail.com>
2024-08-27 13:57:09 +0530

    minute updates related to scroll adjustment

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,6 +4,8 @@ import Carousel from "@/components/Carousel";
 import Header from "@/components/Header";
 import LoaderSpinner from "@/components/LoaderSpinner";
 import { api } from "@/convex/_generated/api";
+import { cn } from "@/lib/utils";
+import { useAudio } from "@/providers/audioProvider";
 import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import Image from "next/image";
@@ -15,9 +17,14 @@ const RightSidebar = () => {
   const router = useRouter();
   const { user } = useUser();
   const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  const { audio } = useAudio();
+
   if (!topPodcasters) return <LoaderSpinner />;
+
   return (
-    <section className="right_sidebar text-white-1">
+    <section className={cn("right_sidebar h-[calc(100vh-5px)]", {
+      'h-[calc(100vh-140px)]': audio?.audioUrl
+    })}>
       <SignedIn>{/*  This Functionality is coming from Clerk */}
         <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
           <UserButton />
